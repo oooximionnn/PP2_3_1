43d87f363db666ec3e1134a91622ce0168078073
oooximionnn <anton.e4maew@gmail.com>
2022-07-19 19:11:19 +0500

    Изменил c xml на джава код

---
+++ src/main/java/com/echmaev/spring/mvc_hibernate/config/ApplicationContext.java
@@ -0,0 +1,37 @@
+package com.echmaev.spring.mvc_hibernate.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import java.util.logging.Filter;
+
+public class ApplicationContext extends AbstractAnnotationConfigDispatcherServletInitializer {
+    // Этот метод должен содержать конфигурации которые инициализируют Beans
+    // для инициализации бинов у нас использовалась аннотация @Bean
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{HibernateConfig.class};
+    }
+
+    // Тут добавляем конфигурацию, в которой инициализируем ViewResolver
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+
+        return new Class<?>[]{
+                Web.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    protected javax.servlet.Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return (javax.servlet.Filter[]) new Filter[] {(Filter) characterEncodingFilter};
+    }
+
+}
---
+++ src/main/java/com/echmaev/spring/mvc_hibernate/config/Web.java
@@ -0,0 +1,24 @@
+package com.echmaev.spring.mvc_hibernate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.echmaev.spring.mvc_hibernate")
+public class Web {
+    @Bean
+    ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
